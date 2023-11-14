6a26aca8c59da9389043c2a95fd5d02aea2e3b34
alvarohv2021 <alvarohv2013@gmail.com>
2023-11-14 22:18:53 +0100

    barra lateral conseguida ?

--- app/src/main/java/com/example/menupropio/MainActivity.java
+++ app/src/main/java/com/example/menupropio/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.menupropio;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
@@ -16,18 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        Toolbar toolbarView = findViewById(R.id.toolbar);
+        DrawerLayout drawerLayoutView = findViewById(R.id.drawer_layout);
+
+        setSupportActionBar(toolbarView);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayoutView, toolbarView, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawerLayoutView.addDrawerListener(toggle);
+        toggle.syncState();
     }
-
-    NavigationView navigationView = findViewById(R.id.nav_view);
-    Toolbar toolbarView = findViewById(R.id.toolbar);
-    DrawerLayout drawerLayoutView = findViewById(R.id.drawer_layout);
-    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 1);
-
-    @Override
-    public void setSupportActionBar(@Nullable androidx.appcompat.widget.Toolbar toolbar) {
-        super.setSupportActionBar(toolbar);
-    }
-    drawerLayoutView.addDrawerListener(toggle);
-    toggle.syncState();
-
 }
